c2f6e646bdca1a288d51de5b307f833b793104b1
Kun Ran <rankun203@gmail.com>
2018-12-27 14:34:21 +0800

    chore: log

--- src/to-samples.ts
+++ src/to-samples.ts
@@ -14,7 +14,7 @@ export function toSamples(
   sampleRate: number = 10
 ): ArrayBuffer {
   if (!testBin()) {
-    throw new Error("audiowaveform is not included in $PATH");
+    throw new Error("audiowaveform is not installed");
   }
 
   const outputDatLoc = getTmpDir();

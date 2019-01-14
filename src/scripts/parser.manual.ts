4421cbecc263b1c1c8c1bb663d08d562cbf1cdbc
Kun Ran <rankun203@gmail.com>
2019-01-14 17:09:05 +0800

    doc: update audio urls

--- src/scripts/parser.manual.ts
+++ src/scripts/parser.manual.ts
@@ -2,12 +2,12 @@ import { parseFile } from "../parser";
 
 /**
  * ```shell
- * node lib/scripts/parser.manual.js https://cdn.vtings.com/4092/posts/1541754712737/2bf55cd0-c229-482f-b4ee-704a73c36a61_convert.mp3
+ * node lib/scripts/parser.manual.js https://cdn.jsdelivr.net/gh/lodestreams/waveform-samples-generator@master/docs/1min.mp3
  * ```
  */
 (async () => {
   let url =
-    "https://cdn.vtings.com/4094/posts/1541836143/2018-11-10_15-45-31.mp3";
+    "https://cdn.jsdelivr.net/gh/lodestreams/waveform-samples-generator@master/docs/1min.mp3";
   if (`${process.argv[2]}`.startsWith("http")) {
     url = process.argv[2];
   }
--- src/scripts/player.manual.ts
+++ src/scripts/player.manual.ts
@@ -6,10 +6,8 @@ import { printBlocks } from "../utils/print-blocks";
   if (`${process.argv[2]}`.startsWith("http")) {
     url = process.argv[2];
   } else {
-    // const url = "https://cdn.vtings.com/4094/posts/1541836143/2018-11-10_15-45-31.mp3";
-    // const url = "https://cdn.vtings.com/4092/posts/1541754712737/2bf55cd0-c229-482f-b4ee-704a73c36a61_convert.mp3";
     url =
-      "https://cdn.vtings.com/4192/posts/1544607685828/02b20645-2507-4da6-a00b-507886d4f871_convert.mp3";
+      "https://cdn.jsdelivr.net/gh/lodestreams/waveform-samples-generator@master/docs/1min.mp3";
   }
   const waveform = await parseFile({ url });
 
